f75547e120b069a1f5bb857a709501b498e7961d
Kasi <isakhansson04@gmail.com>
2025-01-25 11:31:06 +0100

    aksdfjlö

--- lab1/lab1-part1/lab1-part1/main.c
+++ lab1/lab1-part1/lab1-part1/main.c
@@ -1,7 +1,7 @@
 //lab1-part1
 
 #include <avr/io.h>
-
+#include <stdbool.h>
 
 int main(void)
 {
@@ -10,4 +10,22 @@ int main(void)
     {
     }
 }
+
+//pos: 0-5 position of the char, ch: char to display
+void writeChar(char ch, int pos)
+{
+	//check outside range of display
+	if(pos<0 || pos>5)
+	{
+		return;
+	}
+	
+	//replace if not 0-9
+	if (ch < '0' || ch > '9')
+	{
+		ch = ' ';
+	}
+}
+
+
 
