6baac5cb7fe6eb33eb25da39507185862e07ac37
Frossel <askfrossling@hotmail.com>
2025-02-03 15:35:46 +0100

    sa

--- lab2/lab2-part1/lab2-part1/main.c
+++ lab2/lab2-part1/lab2-part1/main.c
@@ -1,18 +1,11 @@
-/*
- * lab2-part1.c
- *
- * Created: 03/02/2025 15:16:48
- * Author : AskFr
- */ 
-
 #include <avr/io.h>
 
 
 int main(void)
 {
     /* Replace with your application code */
-    while (1) 
-    {
+    while (1) {
+		
     }
 }
 
