8bb44eab7d80119a7ba8538c0de758c0c13930f9
Frossel <askfrossling@hotmail.com>
2025-02-16 19:30:21 +0100

    25h156hw

---
+++ lab3/Lab3.3/GccApplication1/GccApplication1/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 16/02/2025 19:30:12
+ * Author : AskFr
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
