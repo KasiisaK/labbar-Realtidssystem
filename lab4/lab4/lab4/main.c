09a8d7f2655f08ba3f5147571021e83dffefafbb
Frossel <askfrossling@hotmail.com>
2025-02-22 17:16:14 +0100

    5n2

---
+++ lab4/lab4/lab4/main.c
@@ -0,0 +1,19 @@
+/*
+ * lab4.c
+ *
+ * Created: 22/02/2025 17:10:33
+ * Author : Frossel
+ */ 
+
+#include <avr/io.h>
+#include "TinyTimber.h"
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
