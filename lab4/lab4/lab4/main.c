c8f1cd0d41be656d1b3fa022b1c68cbbc14f2610
Kasi <isakhansson04@gmail.com>
2025-02-23 13:39:29 +0100

    gui in main

--- lab4/lab4/lab4/main.c
+++ lab4/lab4/lab4/main.c
@@ -1,19 +1,13 @@
-/*
- * lab4.c
- *
- * Created: 22/02/2025 17:10:33
- * Author : Frossel
- */ 
-
 #include <avr/io.h>
+
 #include "TinyTimber.h"
+#include "GUI.h"
 
+//GUI object
+GUI gui = initGUI();
 
-int main(void)
-{
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+int main() {
+    return TINYTIMBER(&gui, updateDisplay, 0);
 }
+
 
