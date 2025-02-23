a62d9d00eef4a3a0f7b39a3834f03ceee91fa61c
Kasi <isakhansson04@gmail.com>
2025-02-23 13:57:53 +0100

    portwrite 1.0

---
+++ lab4/lab4/lab4/PortWrite.h
@@ -0,0 +1,20 @@
+#ifndef PORTWRITE_H
+#define PORTWRITE_H
+
+#include <avr/io.h>
+#include "TinyTimber.h"
+
+
+// Define PortWrite object
+typedef struct {
+    Object super;
+} PortWrite;
+
+// Initialization macro
+#define initPortWrite() { initObject() }
+
+// Function declarations
+void setPin(PortWrite *self, int bit, int value);
+void togglePin(PortWrite *self, int bit);
+
+#endif
