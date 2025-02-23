230a2a61a7f9a78ca08e6c9df4650e1082f43d74
Frossel <askfrossling@hotmail.com>
2025-02-23 16:28:54 +0100

    8y0g

---
+++ lab4/lab4/lab4/Debug/makedep.mk
@@ -0,0 +1,16 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+GUI.c
+
+joystickHandler.c
+
+main.c
+
+PortWrite.c
+
+PulseGen.c
+
+TinyTimber.c
+
