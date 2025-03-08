cb046316b2baac4120f6d3e14492c04d0db27995
Frossel <askfrossling@hotmail.com>
2025-03-08 18:37:13 +0100

    itbv

---
+++ lab5/lab5/lab5/main.c
@@ -0,0 +1,18 @@
+/*
+ * lab5.c
+ *
+ * Created: 08/03/2025 18:14:19
+ * Author : AskFr
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
