6739edf119774a850027ff2799ce8af65c8cd8be
Frossel <askfrossling@hotmail.com>
2025-03-10 10:52:55 +0100

    yw

--- lab5/lab5/lab5/controller/traffic_light.c
+++ lab5/lab5/lab5/controller/traffic_light.c
@@ -1,3 +1,14 @@
-void main() {
-	
+#include <avr/io.h>
+#include "traffic_light.h"
+
+void updateDisplay(TrafficLight *self) {
+	printAt(self->north_queue, self->pos_north_q);
+	printAt(self->south_queue, self->pos_south_q);
+	printAt(self->lamp_state, self->pos_lights);
+}
+
+void releaseCar(TrafficLight *self, int arg) {
+	if (self->cars_on_bridge == 0) {
+		self->direction = !self->direction;
+	}
 }
--- lab5/lab5/lab5/controller/traffic_light.h
+++ lab5/lab5/lab5/controller/traffic_light.h
@@ -2,13 +2,24 @@
 #define TRAFFICLIGHT_H
 
 #include "TinyTimber.h"
+#include "gui.h"
 
 typedef struct {
 	Object super;
+	GUI *gui;
 	uint8_t north_queue, south_queue;
 	uint8_t lamp_state;
+	uint8_t direction;					// 0 = north, 1 = south
+	uint8_t cars_on_bridge;
+	// AVR pos
+	uint8_t pos_north_q;
+	uint8_t pos_south_q;
+	uint8_t pos_lights;
 } TrafficLight;
 
-#define initTrafficLight() { initObject(), 0, 0, 0, 0, 0 }
+void updateLCD(TrafficLight *self);
+void releaseCar(TrafficLight *self, int arg);
+
+#define initTrafficLight() { initObject(), gui, 0, 0, 0, 0, 0 }
 	
 #endif
